99e8bdf1cd99aef9b33cd2677f74a84dc784ffa6
John Henrik Muller <johnhenrikmuller@gmail.com>
2021-02-23 18:51:01 +0100

    Added a bunch of stuff. :D We can log your time on sites now.

--- src/background.js
+++ src/background.js
@@ -15,7 +15,7 @@ chrome.runtime.onInstalled.addListener(({ reason }) => {
 
 function setList(list) {
   chrome.storage.sync.set({ list: list }, function (value) {
-    console.log(value);
+    // console.log(value);
   });
 }
 
@@ -24,26 +24,19 @@ function getList() {
     return data.list;
   });
 }
+let sessionTab;
 let list;
 chrome.storage.sync.get("list", function (data) {
   list = data.list;
-  console.log(list);
+  // console.log(list);
 });
-
-// Old redirection function.
-function redirect(tabId) {
-  chrome.tabs.update(tabId, {
-    url: "https://app.clickup.com/4637248/v/b/li/46667272",
-  });
-  console.log(document.location);
-}
 
 let redirected = false;
 
 chrome.webRequest.onBeforeRequest.addListener(
   function (details) {
     redirected = true;
-    console.log(getList());
+    // console.log(getList());
     return {
       redirectUrl:
         // "https://app.clickup.com/4637248/v/b/li/46667048"
@@ -58,17 +51,18 @@ chrome.webRequest.onBeforeRequest.addListener(
       "*://www.reddit.com/*",
       "*://www.9gag.com/*",
     ],
-    // [...list],
-    // [...getList()],
   },
   ["blocking"]
 );
 
+//Working solution, may be a little heavy at the moment.
+//Need to find some way to remove eventlistener again. Examples in Aiki.
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log(changeInfo);
+  // console.log(changeInfo);
   if (changeInfo.status === "complete" && redirected) {
     sendMessage(tabId, "www.codecademy.com");
     redirected = false;
+    sessionTab = tab;
     let host = tab.url.split("/")[2];
     chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
       if (host == tab.url.split("/")[2]) {
@@ -78,24 +72,93 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   }
 });
 
-/* Did not work with codecademy. Seems the event does not fire automatically */
-// chrome.webNavigation.onDOMContentLoaded.addListener(function (details) {
-//   if (redirected) {
-//     sendMessage(details.tabId, "www.codecademy.com");
-//     redirected = false;
-//   }
-// });
-
-/* Did not work with codecademy. Seems the event does not fire automatically */
-// chrome.webNavigation.onCompleted.addListener(function (details) {
-//   if (redirected) {
-//     sendMessage(details.tabId, "www.codecademy.com");
-//     redirected = false;
-//   }
-// });
-
 function sendMessage(tabId, message) {
   chrome.tabs.sendMessage(tabId, { message: message }, function (response) {
-    console.log(response.message);
+    // console.log(response.message);
   });
 }
+
+function logWhatever(string) {
+  const currentDate = new Date();
+  const timestamp = currentDate.now;
+  // console.log(timestamp + " " + string);
+}
+
+let beginTime;
+let endTime;
+
+
+//When you load codecademy.
+// chrome.webNavigation.onCompleted.addListener((details) => {
+
+//   if (details.frameId == '0') {
+//     let host = details.url.split("/")[2];
+
+//     console.log("This is the host: " + host);
+    
+//       //If the host is Codecademy, we start a timer.
+//       if (host = "www.codecademy.com") {
+//         beginTime = new Date().toLocaleString();
+
+//         //We start a new listener that detects when we navigate away from the page, and logs an end time.
+//         chrome.webNavigation.onCompleted.addListener((details) => {
+//           if (details.frameId == '0'){
+//             let host2 = details.url.split("/")[2];
+
+//             if (host2 !== "www.codecademy.com") {
+//               endTime = new Date().toLocaleString();
+//               console.log("The begin time was: " + beginTime);
+//               console.log("The end time was: " + endTime);
+//             }
+//         }})
+//       }
+//   }
+// });
+
+chrome.webNavigation.onCompleted.addListener(startSession);
+
+/*Fires when the active tab in a window changes. Note that the tab's URL may not be set at the time this event fired,
+but you can listen to onUpdated events so as to be notified when a URL is set.*/
+chrome.tabs.onActivated
+
+/* */
+chrome.tabs.onRemoved
+
+function startSession(details) {
+    if (details.frameId == '0') {
+      let host = details.url.split("/")[2];
+  
+      console.log("This is the host: " + host);
+      
+        //If the host is Codecademy, we start a timer.
+        if (host = "www.codecademy.com") {
+          beginTime = new Date();
+        }
+      
+      chrome.webNavigation.onCompleted.addListener(endSession);
+      chrome.webNavigation.onCompleted.removeListener(startSession);
+  }
+};
+
+function endSession(details) {
+    if (details.frameId == '0'){
+      let host = details.url.split("/")[2];
+
+      if (host !== "www.codecademy.com") {
+        endTime = new Date();
+        console.log("The begin time was: " + beginTime.toLocaleString());
+        console.log("The end time was: " + endTime.toLocaleString());
+
+        chrome.webNavigation.onCompleted.removeListener(endSession);
+        chrome.webNavigation.onCompleted.addListener(startSession);
+        console.log(sessionLength(endTime,beginTime, host));
+      }
+    }
+};
+
+
+function sessionLength(date1, date2, host) {
+  let time = date1.getTime() - date2.getTime();
+  return("You spent " + Math.floor(time/1000) + " seconds on " + host);
+}
+
