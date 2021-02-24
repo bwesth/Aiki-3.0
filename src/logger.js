54f67f8aff2217bf9af9697e23a9318b352bc617
John Henrik Muller <johnhenrikmuller@gmail.com>
2021-02-24 13:37:16 +0100

    Added logger class.

---
+++ src/logger.js
@@ -0,0 +1,23 @@
+//Logging functions go here
+
+function logAction (actor, time, action, sessionStatus) {
+    console.log(actor + " performed this " + action + " at this " + time);
+}
+
+//JOHN PROCRASTINATES at www.facebook.com AT 15.25
+//JOHN INTERCEPTED to www.codecademy.com AT 15.25
+//JOHN COMPLETED session at www.codeacademy.com AT 15.30 
+//SESSION LENGTH 00:05 mins 
+
+//JOHN PROCRASTINATES at www.facebook.com AT 15.25
+//JOHN INTERCEPTED to www.codecademy.com AT 15.25
+//JOHN SKIPPED session at www.codeacademy.com AT 15.28 
+//SESSION LENGTH 00:03 mins 
+
+//JOHN PROCRASTINATES at www.facebook.com AT 15.25
+//JOHN INTERCEPTED to www.codecademy.com AT 15.25
+//JOHN ABORTED session at www.codeacademy.com AT 15.25 ABORT METHOD: (CLOSED_TAB)
+//SESSION LENGTH 00:00 mins 
+
+
+
