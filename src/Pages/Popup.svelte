27ef6dfa08696daa11df127054a8bc067ea6a76b
Bjørn Westh <bjwe@itu.dk>
2021-03-02 13:03:13 +0100

    Included bound inputs

--- src/Pages/Popup.svelte
+++ src/Pages/Popup.svelte
@@ -1,5 +1,20 @@
 <!-- Popup component that is painted when user clicks the extension icon in chrome extensions menu -->
 <script>
+  	import browser from "webextension-polyfill";
+	import Input from './Components/Input.svelte';
+	let list;
+	chrome.storage.sync.get("list", function (data) {
+		list = data.list;
+		console.log(list);
+	});
+	function setList() {
+      chrome.storage.sync.set({ list: list }, function (value) {
+        console.log(value);
+      });
+	  console.log(list)
+    }
+
+
 /* Opens a new tab with options page and selects it */
 function background () {
     chrome.tabs.create({
@@ -9,7 +24,15 @@ function background () {
 }
 </script>
 
-
+<main>
+	<h3>These are your registered procrastination sites:</h3>
+	{#if list !== undefined}
+		{#each list as item}
+			<Input bind:value={item}/>
+		{/each}	
+	{/if}
+	<button on:click={setList}>Save list</button>
+</main>
 
 
 <button on:click={() => background()}>Go to background</button>
