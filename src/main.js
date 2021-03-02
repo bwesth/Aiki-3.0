444ebcc517de7bfa1af806ffcc084b0ddaf68c6c
Bjørn Westh <bjwe@itu.dk>
2021-03-02 10:45:40 +0100

    first steps

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-import App from './App.svelte';
+import Popup from './Pages/Popup.svelte';
+import Options from './Pages/Options.svelte'
 
-const app = new App({
+function Router() {
+let params = (new URL(window.location)).searchParams; // since chrome 51, no IE
+
+    switch (params.get('page')) {
+        case 'options':
+            return <Options />;
+        default:
+            return <Popup />;
+    }
+}
+
+const app = new Router({
 	target: document.body
 });
 
