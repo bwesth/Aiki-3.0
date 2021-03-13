79fd919d24d19a16aeaa22c68ac45ca690625226
Bjørn Westh <bjwe@itu.dk>
2021-03-13 17:21:51 +0100

    cleanup++

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -1,30 +1,97 @@
 import { logProcrastinationEvent, logLearningEvent } from "./util/logger.js";
 import storage from "./util/storage";
 import { learningSites } from "./util/constants";
+
 let currentName;
+let user;
+let procrastinationSites;
+
+function updateProcrastinationSites() {
+  storage.getList((list) => {
+    procrastinationSites = list.map((site) => site.name);
+  });
+}
+
+function updateUser() {
+  storage.getUID((uid) => {
+    user = uid;
+  });
+}
+
+updateUser();
+updateProcrastinationSites();
 
 function userOnSite(details) {
-  storage.getList((procNameList) => {
-    let list = procNameList.map((site) => site.name);
-    console.log(list);
-    if (details.frameId == "0") {
-      let name = parseUrlToName(details.url);
-        if (list.includes(name)) {
+  // Checking if top frame (loads of different frames load on major sites)
+  if (details.frameId == "0") {
+    let name = parseUrlToName(details.url);
+    // Getting list of the active tabs in each chrome window
+    chrome.tabs.query({ active: true }, (response) => {
+      // Just need the IDs
+      const activeTabIds = response.map((window) => window.id);
+      console.log(activeTabIds.includes(details.tabId));
+      // Checking if the event tab is active in any open chrome window.
+      // If not, the site was opened in a non-active widnow (like when you use middle-mouse-button)
+      if (activeTabIds.includes(details.tabId)) {
+        if (procrastinationSites.includes(name)) {
+          // Procrastination session started
           currentName = name;
-          storage.getUID((user) => {
-            logProcrastinationEvent({
-              tag: "SESSIONSTART",
-              name: currentName || "",
-              user: user,
-              navigationType: "Web Navigation",
-              eventDetails: details,
-            });
-            configSessionStartListeners();
+          logProcrastinationEvent({
+            tag: "SESSIONSTART",
+            name: currentName || "",
+            user: user,
+            navigationType: "Web Navigation",
+            eventDetails: details,
           });
+          configSessionStartListeners();
         }
         if (learningSites.includes(name)) {
+          // learning session started
           currentName = name;
-          storage.getUID((user) => {
+          logLearningEvent({
+            tag: "SESSIONSTART",
+            name: currentName || "",
+            user: user,
+            navigationType: "Web Navigation",
+            eventDetails: details,
+          });
+          configSessionStartListeners();
+        }
+      }
+    });
+  }
+}
+
+function userLeftSite(details) {
+  // Checking if top frame (loads of different frames load on major sites)
+  if (details.frameId == "0") {
+    let name = parseUrlToName(details.url);
+    // Is the name of the new site the same as the name of the current session site?
+    if (name !== currentName) {
+      // If so, we need to add check if it the event fires in the active tab.
+      // If not it is a site opened in another tab that the user is not currently on.
+      chrome.tabs.query({ active: true }, (response) => {
+        const activeTabIds = response.map((window) => window.id);
+        // This is the mentioned active tab check
+        if (activeTabIds.includes(details.tabId)) {
+          const event = {
+            tag: "SESSIONEND",
+            name: currentName || "",
+            user: user,
+            navigationType: "Web Navigation",
+            eventDetails: details,
+          };
+          // If current session name is a learning site, log that, if not, it's a procrastination site
+          if (learningSites.includes(currentName)) {
+            logLearningEvent(event);
+          } else {
+            logProcrastinationEvent(event);
+          }
+
+          // Now we need to check if the new site is a different learning or procrastination site.
+          if (learningSites.includes(name)) {
+            // New learning session initiated
+            currentName = name;
             logLearningEvent({
               tag: "SESSIONSTART",
               name: currentName || "",
@@ -32,64 +99,25 @@ function userOnSite(details) {
               navigationType: "Web Navigation",
               eventDetails: details,
             });
-            configSessionStartListeners();
-          });
-        }
-    }
-  });
-}
-
-function userLeftSite(details) {
-  if (details.frameId == "0") {
-    let name = parseUrlToName(details.url);
-    chrome.tabs.query({ active: true }, (response) => {
-      if (name !== currentName && details.tabId == response[0].id) {
-        storage.getList((procNameList) => {
-          let list = procNameList.map((site) => site.name);
-
-          //Need to add check for active tab
-          storage.getUID((user) => {
-            const event = {
-              tag: "SESSIONEND",
+          } else if (procrastinationSites.includes(name)) {
+            // New procrastination session initiated
+            currentName = name;
+            logProcrastinationEvent({
+              tag: "SESSIONSTART",
               name: currentName || "",
               user: user,
               navigationType: "Web Navigation",
               eventDetails: details,
-            };
-            if (learningSites.includes(currentName)) {
-              logLearningEvent(event);
-            } else {
-              logProcrastinationEvent(event);
-            }
-            if (learningSites.includes(name)) {
-              //New learning session initiated
-              currentName = name;
-              logLearningEvent({
-                tag: "SESSIONSTART",
-                name: currentName || "",
-                user: user,
-                navigationType: "Web Navigation",
-                eventDetails: details,
-              });
-            } else if (list.includes(name)) {
-              // New procrastination session initiated
-              currentName = name;
-              logProcrastinationEvent({
-                tag: "SESSIONSTART",
-                name: currentName || "",
-                user: user,
-                navigationType: "Web Navigation",
-                eventDetails: details,
-              });
-            } else {
-              // Neither procrastination nor learning session, not creating session.
-              configSessionEndListeners();
-              currentName = undefined;
-            }
-          });
-        });
-      }
-    });
+            });
+          } else {
+            // Neither procrastination nor learning session, not creating new session,
+            // but instead finishing closing the original session.
+            configSessionEndListeners();
+            currentName = undefined;
+          }
+        }
+      });
+    }
   }
 }
 
@@ -104,153 +132,140 @@ function userActivatesTab(details) {
 
 function addOnWindowsCloseListener() {
   chrome.windows.onRemoved.addListener((details) => {
-    storage.getUID((user) => {
-      const event = {
-        tag: "SESSIONEND",
-        name: currentName || "",
-        user: user,
-        navigationType: "Closed chrome window",
-        eventDetails: details,
-      };
-      if (learningSites.includes(currentName)) {
-        logLearningEvent(event);
-      } else {
-        logProcrastinationEvent(event);
-      }
-      configSessionEndListeners();
-    });
+    const event = {
+      tag: "SESSIONEND",
+      name: currentName || "",
+      user: user,
+      navigationType: "Closed chrome window",
+      eventDetails: details,
+    };
+    if (learningSites.includes(currentName)) {
+      logLearningEvent(event);
+    } else {
+      logProcrastinationEvent(event);
+    }
+    configSessionEndListeners();
   });
 }
 
 addOnWindowsCloseListener();
 
 function tabActivatedCallback(response, details) {
-  storage.getUID((user) => {
-    // This is a lot of spagetti
-    if (response.host) {
-      console.log("Getting response:");
-      console.log(response);
-      // Just want the name of the website, not the scheme or any fragments.
-      let nameOfNewTab = parseUrlToName(response.host);
-      if (!currentName) {
+  // This is a lot of spagetti
+  if (response.host) {
+    console.log("Getting response:");
+    console.log(response);
+    // Just want the name of the website, not the scheme or any fragments.
+    let nameOfNewTab = parseUrlToName(response.host);
+    if (!currentName) {
+      console.log(
+        "currentName is undefined, checking against list of procrastination sites"
+      );
+      //If current name IS undefined, then we MAY be arriving at a procrastination site now.
+      if (procrastinationSites.includes(nameOfNewTab)) {
         console.log(
-          "currentName is undefined, checking against list of procrastination sites"
+          nameOfNewTab + " is on the list. Procrastination session started."
         );
-        //If current name IS undefined, then we MAY be arriving at a procrastination site now.
-        storage.getList((procNameList) => {
-          let list = procNameList.map((site) => site.name);
-          console.log(list);
-          if (list.includes(nameOfNewTab)) {
-            console.log(
-              nameOfNewTab + " is on the list. Procrastination session started."
-            );
-            currentName = nameOfNewTab;
-            //If the name of the new tab is in our procNameList, then we need to start a new proc session.
-            configSessionStartListeners();
-            logProcrastinationEvent({
-              tag: "SESSIONSTART",
-              name: currentName || "",
-              user: user,
-              navigationType: "Tab change",
-              eventDetails: details,
-            });
-          } else if (learningSites.includes(nameOfNewTab)) {
-            // Learning site session
-            currentName = nameOfNewTab;
-            configSessionStartListeners();
-            logLearningEvent({
-              tag: "SESSIONSTART",
-              name: currentName || "",
-              user: user,
-              navigationType: "Tab change",
-              eventDetails: details,
-            });
-          } else {
-            console.log(
-              nameOfNewTab +
-                " is not on the list. This is not a procrastination site."
-            );
-          }
+        currentName = nameOfNewTab;
+        //If the name of the new tab is in our procNameList, then we need to start a new proc session.
+        configSessionStartListeners();
+        logProcrastinationEvent({
+          tag: "SESSIONSTART",
+          name: currentName || "",
+          user: user,
+          navigationType: "Tab change",
+          eventDetails: details,
+        });
+      } else if (learningSites.includes(nameOfNewTab)) {
+        // Learning site session
+        currentName = nameOfNewTab;
+        configSessionStartListeners();
+        logLearningEvent({
+          tag: "SESSIONSTART",
+          name: currentName || "",
+          user: user,
+          navigationType: "Tab change",
+          eventDetails: details,
         });
       } else {
         console.log(
-          "currentName is defined, checking against activated tab for change of active website"
+          nameOfNewTab +
+            " is not on the list. This is not a procrastination site."
         );
+      }
+    } else {
+      console.log(
+        "currentName is defined, checking against activated tab for change of active website"
+      );
 
-        //If current name is not "undefined", then that means our current TAB is a procrastination site.
-        if (currentName !== nameOfNewTab) {
-          console.log(
-            "currentName is not same as new tab name. Ending session"
-          );
-          //If currentName is not the same as the name of the NEW tab, we need to end the session of CurrentName.
+      //If current name is not "undefined", then that means our current TAB is a procrastination site.
+      if (currentName !== nameOfNewTab) {
+        console.log("currentName is not same as new tab name. Ending session");
+        //If currentName is not the same as the name of the NEW tab, we need to end the session of CurrentName.
 
-          // Need to test if the new tab is a procrastination site, then create a new session without closing the listeners
-          storage.getList((procNameList) => {
-            let list = procNameList.map((site) => site.name);
-            const event = {
-              tag: "SESSIONEND",
-              name: currentName || "",
-              user: user,
-              navigationType: "Tab change",
-              eventDetails: details,
-            };
-            if (learningSites.includes(currentName)) {
-              logLearningEvent(event);
-            } else {
-              logProcrastinationEvent(event);
-            }
-            if (list.includes(nameOfNewTab)) {
-              console.log("new tab is also a procrastination site.");
-              currentName = nameOfNewTab;
-              logProcrastinationEvent({
-                tag: "SESSIONSTART",
-                name: currentName || "",
-                user: user,
-                navigationType: "Tab change",
-                eventDetails: details,
-              });
-            } else if (learningSites.includes(nameOfNewTab)) {
-              currentName = nameOfNewTab;
-              logLearningEvent({
-                tag: "SESSIONSTART",
-                name: currentName || "",
-                user: user,
-                navigationType: "Tab change",
-                eventDetails: details,
-              });
-            } else {
-              console.log(
-                "New tab is not a new procrastination site. Not making a new session."
-              );
-              configSessionEndListeners();
-              currentName = undefined;
-            }
+        // Need to test if the new tab is a procrastination site, then create a new session without closing the listeners
+        const event = {
+          tag: "SESSIONEND",
+          name: currentName || "",
+          user: user,
+          navigationType: "Tab change",
+          eventDetails: details,
+        };
+        if (learningSites.includes(currentName)) {
+          logLearningEvent(event);
+        } else {
+          logProcrastinationEvent(event);
+        }
+        if (procrastinationSites.includes(nameOfNewTab)) {
+          console.log("new tab is also a procrastination site.");
+          currentName = nameOfNewTab;
+          logProcrastinationEvent({
+            tag: "SESSIONSTART",
+            name: currentName || "",
+            user: user,
+            navigationType: "Tab change",
+            eventDetails: details,
+          });
+        } else if (learningSites.includes(nameOfNewTab)) {
+          currentName = nameOfNewTab;
+          logLearningEvent({
+            tag: "SESSIONSTART",
+            name: currentName || "",
+            user: user,
+            navigationType: "Tab change",
+            eventDetails: details,
           });
         } else {
-          console.log("CurrentTab same as new tab, session not broken.");
+          console.log(
+            "New tab is not a new procrastination site. Not making a new session."
+          );
+          configSessionEndListeners();
+          currentName = undefined;
         }
-      }
-    } else if (currentName) {
-      console.log(
-        response.message +
-          "\nActive tab is not a website. Might be a new empty tab or similar"
-      );
-      configSessionEndListeners();
-      const event = {
-        tag: "SESSIONEND",
-        name: currentName || "",
-        user: user,
-        navigationType: "Tab change",
-        eventDetails: details,
-      };
-      if (learningSites.includes(currentName)) {
-        logLearningEvent(event);
       } else {
-        logProcrastinationEvent(event);
+        console.log("CurrentTab same as new tab, session not broken.");
       }
-      currentName = undefined;
     }
-  });
+  } else if (currentName) {
+    console.log(
+      response.message +
+        "\nActive tab is not a website. Might be a new empty tab or similar"
+    );
+    configSessionEndListeners();
+    const event = {
+      tag: "SESSIONEND",
+      name: currentName || "",
+      user: user,
+      navigationType: "Tab change",
+      eventDetails: details,
+    };
+    if (learningSites.includes(currentName)) {
+      logLearningEvent(event);
+    } else {
+      logProcrastinationEvent(event);
+    }
+    currentName = undefined;
+  }
 }
 
 function addOnSiteListeners() {
