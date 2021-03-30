6b92c821287cfd927118c8ac1f38d173594dbef1
John Henrik Muller <johnhenrikmuller@gmail.com>
2021-03-30 10:33:52 +0200

    Fixed text in settings menu.

--- src/Pages/Components/Settings/Privacy.svelte
+++ src/Pages/Components/Settings/Privacy.svelte
@@ -6,16 +6,19 @@ import SettingsContainer from "./SettingsContainer.svelte";
 <h5>We want you to know we take your privacy very seriously:</h5>
 <hr>
 
-<p>For the duration of this study, we will be logging the amount of time you spend on websites that you add to the extension as procrastination 
-    sites. However, we will only log the time spent on each site, nothing more. We will not know what you visited on those pages, only name of 
-    the root site.</p>
+<p>For the duration of this study, Aiki will be logging the amount of time you spend on websites that
+    you add to it as procrastination sites. However, Aiki will only log the time spent (in seconds) on each 
+    site, nothing more. Aiki will not know anything about the content of those pages, only name of the root site 
+    (ie: Youtube.com, Facebook.com, etc).</p>
 
-<p><strong>For example:</strong> The extension logs that you spent 45 seconds on Facebook.com on Tuesday, but absolutely nothing about the content 
-    of the Facebook pages you had open. We will later use this data to see if our extension helped you to procrastinate a little less.</p>
+<p><strong>For example:</strong> Aiki would log that you spent 45 seconds on Facebook.com on Tuesday, but absolutely 
+    nothing about the content of the Facebook pages you had open. We will later use this data to see if Aiki 
+    helped you to procrastinate a little less.</p>
 
-<p>In an effort to increase security, we will be tying your data to a User ID (UID) which is a unique number we will use to identify you. 
-    No one but the research team will know which UID links to which identity, rendering your data anonymous to anyone else. 
-    Your real identity will never be used or mentioned in any of the following research.</p>
+<p>In an effort to increase security, Aiki will save your data by generating a User ID (UID) from your email. 
+    Your UID is a unique number the research team will use to identify you. No one but the research team will 
+    know which UID links to which identity, rendering your data anonymous to anyone else. Your real identity 
+    will never be used or mentioned in any of the following research.</p>
 </SettingsContainer>
 
 <style>
--- src/Pages/Components/Settings/SetUser.svelte
+++ src/Pages/Components/Settings/SetUser.svelte
@@ -6,7 +6,7 @@
   import { makeDate } from '../../../util/utilities'
 
   import Fa from 'svelte-fa'
-  import { faUser, faUserSlash, faUserPlus} from '@fortawesome/free-solid-svg-icons'
+  import { faUserSlash, faUserPlus} from '@fortawesome/free-solid-svg-icons'
 
   export let user = "";
   export let userIsRegistered;
@@ -18,7 +18,7 @@
   }
   
   function confirmUid(){
-    const confirmation = confirm("Are you certain the user ID is correct?");
+    const confirmation = confirm("Are you certain the provided email is correct?");
     if (confirmation) {
       storage.setUid(user);
       let date = makeDate()
@@ -33,7 +33,7 @@
   }
 
   function resetUid() {
-    const confirmation = confirm("Are you certain you want to reset your UID?");
+    const confirmation = confirm("Are you certain you want to reset your email?");
     if (confirmation) {
       firebase.addLog({
         user: user,
@@ -57,14 +57,14 @@
       <input class="form-control" type="text" placeholder={user} readonly>
       <button class="btn btn-danger" on:click={resetUid}><Fa icon={faUserSlash}/> Reset User ID</button>
     {:else}
-    <h5>Add your UID here so we can log your activity:</h5>
+    <h5>Add your email here so we can log your activity:</h5>
     <hr>
-    <p><strong>Note:</strong> Please make sure you enter the correct UID provided to you by email. If you provide the incorrect one, your
-      data is likely to become mixed up with another participant.</p>
-    <p>Secondly, please note that you may be asked to re-enter your UID if you clear your cache or browser history, in order
+    <p><strong>Note:</strong> Please make sure you enter the correct email you have been using so far for the study. 
+      If you provide the incorrect one, your data is likely to become mixed up with another participant.</p>
+    <p>Secondly, please note that you may be asked to re-enter your email if you clear your cache or browser history, in order
         for us to resume logging.</p>
-    <p>If you have not recieved a UID, or have misplaced yours, contact <a href="mailto:aiki.itu.info@gmail.com">aiki.itu.info@gmail.com</a> 
-      for assistance</p>
+    <p>If you have any questions or problems, contact <a href="mailto:aiki.itu.info@gmail.com">aiki.itu.info@gmail.com</a> 
+      for assistance.</p>
 
     <hr>
     <!-- Bootstrap Input field. -->
