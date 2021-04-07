3e5c85d83c45c18f6cc50e113d8a1144c59a4a0b
John Henrik Muller <johnhenrikmuller@gmail.com>
2021-04-07 14:52:41 +0200

    Modified privacy text.

--- src/Pages/Components/Settings/Privacy.svelte
+++ src/Pages/Components/Settings/Privacy.svelte
@@ -5,15 +5,16 @@ import SettingsContainer from "./SettingsContainer.svelte";
 <SettingsContainer headline="Your Privacy">
 <h5>We want you to know we take your privacy very seriously:</h5>
 <hr>
-
 <p>For the duration of this study, Aiki will be logging the amount of time you spend on websites that
     you add to it as procrastination sites. However, Aiki will only log the time spent (in seconds) on each 
-    site, nothing more. Aiki will not know anything about the content of those pages, only the name of the root site 
-    (ie: Youtube.com, Facebook.com, etc).</p>
+    procrastination site, nothing more. Aiki will not know anything about any webpages you do not add to it.
+    As for the the content of the pages you do add, only the name of the root site (ie: Youtube.com, 
+    Facebook.com, etc).</p>
 
-<p><strong>For example:</strong> Aiki would log that you spent 45 seconds on Facebook.com on Tuesday, but absolutely 
-    nothing about the content of the Facebook pages you had open. We will later use this data to see if Aiki 
-    helped you to procrastinate a little less.</p>
+<p><strong>For example:</strong> You added Facebook.com as a procrastination site to Aiki. You then visited 
+    Facebook for 45 seconds on Tuesday. Aiki would log that you spent 45 seconds on Facebook.com on Tuesday, 
+    but absolutely nothing else. To be clear, Aiki will not know anything about the content of the pages you 
+    visit. We will later use this data to see if Aiki helped you to procrastinate a little less.</p>
 </SettingsContainer>
 
 <style>
