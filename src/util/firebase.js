34e182d13cad6929928cb7c1d560fec8a55b9524
Bjørn Westh <bjwe@itu.dk>
2021-04-19 09:18:17 +0200

    Couple of docs added

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -16,6 +16,14 @@ firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 
+/**
+ * @async 
+ * @function
+ * @param {DocumentReference} ref A reference to a firestore document
+ * @description Checks if referenced document has a field. If not, it creates a field (active = true).
+ * This serves the purpose of later being able to get a list of all documents in a collection.
+ * If a document does not contain a field, it is not seen as a document by firestore, even if it contains a subcollection.
+ */
 async function resolveDoc(ref) {
   const res = await ref.get();
   const active = res.data();
@@ -24,6 +32,14 @@ async function resolveDoc(ref) {
   }
 }
 
+/**
+ * @async 
+ * @function
+ * @param {object} entry The data to be stored in firestore.
+ * @param {DocumentReference} reference A reference to the nested document which should contain the entry.
+ * @param {string} type Type of entry choosing from "config" | "session" | "redirection"
+ * @description Takes a document reference and adds the entry as a document within a collection specified by type.
+ */
 async function addEntry(entry, reference, type) {
   const entryRes = await reference
     .collection(type + "_logs")
@@ -31,6 +47,15 @@ async function addEntry(entry, reference, type) {
     .set(entry, { merge: true });
 }
 
+/**
+ * @async 
+ * @function
+ * @param {object} entry 
+ * @param {string} type 
+ * @description Top level function to add log entries to firestore. 
+ * Takes entry object and type of entry and resolves each level of firestore document/collection used.
+ * Finally adds entry at appropriate log type within the appropriate date collection.
+ */
 async function addLog(entry, type) {
   entry.user = `${hash(entry.user)}`;
   const userRef = db.collection("user_logs").doc(entry.user);
