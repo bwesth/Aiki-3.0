10d040b38066cc3fb6a1f3179f6bc9f6915a7280
John Henrik Muller <jhmu@itu.dk>
2021-05-05 20:44:27 +0200

    New chart code.

--- src/Pages/Components/Settings/Chart.svelte
+++ src/Pages/Components/Settings/Chart.svelte
@@ -1,63 +1,89 @@
 <script>
   import { onMount } from "svelte";
-  import Chart from 'chart.js/auto';
+  import Chart from "chart.js/auto";
   function createPieChart() {
     var ctx = document.getElementById("pie-chart");
     let myChart = new Chart(ctx, {
       type: "pie",
       data: {
-        labels: ["www.codecademy.com", "www.facebook.com", "www.youtube.com", "www.kotaku.com", "www.reddit.com"],
+        labels: [
+          "Codecademy", //ORDER MATTERS, USE NAMES, NOT URLS
+          "Facebook",
+          "YouTube",
+          "Kotaku",
+          "Reddit",
+          "Other",
+        ],
         datasets: [
           {
-            label: "Time (seconds)",
             backgroundColor: [
-              "#3e95cd",
-              "#8e5ea2",
-              "#3cba9f",
-              "#e8c3b9",
-              "#c45850"
+              //ORDER MATTERS
+              "#0077b6",
+              "#d00000",
+              "#dc2f02",
+              "#e85d04",
+              "#f48c06",
+              "#faa307",
             ],
-            data: [2478, 5267, 734, 784, 433]
-          }
-        ]
+            data: [5267, 2478, 734, 784, 433, 200], //ORDER MATTERS
+            hoverOffset: 8,
+          },
+        ],
       },
       options: {
         plugins: {
-            legend: {
-                display: true,
-                position: 'bottom',
-                labels: {
-                    color: '#FFFFFF',
-                }
-            }
+          title: {
+            text: "Time Spent",
+            color: "#FFFFFF",
+            size: 16,
+            display: true,
+            padding: 10,
+          },
+          legend: {
+            display: true,
+            position: "bottom",
+            align: "center",
+            labels: {
+              color: "#FFFFFF",
+              boxWidth: 12,
+              padding: 12,
+            },
+          },
         },
         layout: {
-            padding: 20,
-            scale: 40
-        }
-      }
+          padding: 10,
+        },
+      },
     });
   }
   onMount(createPieChart);
 </script>
 
-<div class="chart">
-  <canvas id="pie-chart" />
+<div class="flex">
+  <div class="chart">
+    <canvas id="pie-chart" />
+  </div>
 </div>
 
 <style>
-
-    canvas {
-        display: flex;
-        width: 100px;
-        height: 100px;
-    }
-
-  .chart {
-    margin: 1rem 1rem 0 0;
-    background-color: var(--backgroundColorSecondary);
-    width: 100%;
+  canvas {
     display: flex;
     justify-content: center;
+    width: 1px;
+    height: 1px;
+    background-color: #212121;
+  }
+
+  .flex {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .chart {
+    display: flex;
+    background-color: var(--backgroundColorSecondary);
+    width: 50vh;
+    height: 50vh;
   }
 </style>
