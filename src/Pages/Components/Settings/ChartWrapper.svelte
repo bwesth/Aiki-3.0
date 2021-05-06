623b2cdda50b1a054196cdfcb7695640042b0d3f
John Henrik Muller <jhmu@itu.dk>
2021-05-06 12:37:48 +0200

    Made a chart wrapper.

---
+++ src/Pages/Components/Settings/ChartWrapper.svelte
@@ -0,0 +1,53 @@
+<script>
+  // Component imports
+  import Chart from "./Chart.svelte";
+  //Accepts a data object and a type string passed down from statistics.svelte
+  export let data;
+  export let type;
+
+  let skips, completed, snoozes, procTime, learnTime;
+
+  //Getting all variables from data to pipe into the graph, depending on the type.
+  function populateChart() {
+    switch (type) {
+      case "today":
+        skips = data.skipCount;
+        completed = data.completedCount;
+        snoozes = data.snoozeCount;
+        procTime = data.sessionData.procrastinationDuration;
+        learnTime = data.sessionData.learningDuration;
+        break;
+      case "yesterday":
+        skips = data.yesterday.skipCount;
+        completed = data.yesterday.completedCount;
+        snoozes = data.yesterday.snoozeCount;
+        procTime = data.yesterday.sessionData.procrastinationDuration;
+        learnTime = data.yesterday.sessionData.learningDuration;
+        break;
+      case "history":
+        skips = data.history.skipCount;
+        completed = data.history.completedCount;
+        snoozes = data.history.snoozeCount;
+        procTime = data.history.sessionData.procrastinationDuration;
+        learnTime = data.history.sessionData.learningDuration;
+        break;
+      default:
+        skips = 0;
+        completed = 0;
+        snoozes = 0;
+        procTime = 0;
+        learnTime = 0;
+    }
+  }
+
+  populateChart();
+
+  let stats = [learnTime, procTime, completed, skips, snoozes];
+</script>
+
+<div>
+  <Chart {stats} />
+</div>
+
+<style>
+</style>
