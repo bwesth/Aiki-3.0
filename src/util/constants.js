a6c25af0b3946d45302cfa696efc39ca6e083aa2
Bjørn Westh <bjwe@itu.dk>
2021-05-21 11:04:13 +0200

    fluglemaggle

--- src/util/constants.js
+++ src/util/constants.js
@@ -4,4 +4,6 @@ export const learningSites = [
   {name: "udemy", host: "www.udemy.com"},
 ];
 
-export const participantResource = {name: "udemy", host: "www.udemy.com"}
+export const participantResource = {name: "codecademy", host: "www.codecademy.com"}
+// export const participantResource = {name: "sololearn", host: "www.sololearn.com"}
+// export const participantResource = {name: "udemy", host: "www.udemy.com"}
