7d3df6444cdd49deb5894b914322ad02e046797a
bwesth <b.a.westh@gmail.com>
2021-08-30 10:21:49 +0200

    Secrets

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -3,13 +3,13 @@ import "firebase/firebase-firestore";
 import { hash } from "./security";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBY03Fx1mT9Oe2qmP4gcLJlWTF5nXVibRA",
-  authDomain: "aiki3-e209a.firebaseapp.com",
-  projectId: "aiki3-e209a",
-  storageBucket: "aiki3-e209a.appspot.com",
-  messagingSenderId: "664157650460",
-  appId: "1:664157650460:web:244badc7671a88cf3fe9d9",
-  measurementId: "G-GNDXYW96DS",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 firebase.initializeApp(firebaseConfig);
